4d26f2494b74e719bb9d200fc6c648159e46b28d
Mohit Prajapati <promohitprajapati@gmail.com>
2024-08-20 21:56:30 +0530

    Conditional Statement Questions

---
+++ first.py
@@ -0,0 +1,156 @@
+# s1 = "Mohit"
+# s2 = "Prajapati"
+
+# concatinated = s1 + " " + s2
+# print(concatinated)
+# # length = len(s1)
+# # print("length of string"  length)
+# uppercase = s1.upper()
+# uppercase = s2.upper()
+# print(uppercase)
+# lowercase = s1.lower()
+# print(lowercase)
+# titlecase = s1.title()
+# print(titlecase)
+# replaced = s1.replace("M" , "R")
+# print(replaced)
+
+#Boolean
+# is_active = True
+# is_admin = False
+
+# and_result = is_active and is_admin
+# or_result = is_active or is_admin
+# not_result = not is_active 
+# print(and_result)
+# print(or_result)
+# print(not_result)
+
+
+#List
+
+# Fruits = ["apple" , "banana" , "cherry"]
+# print(Fruits[0])
+# print(Fruits[2])
+
+# MOdify
+
+# Fruits[1] = "blueberry"
+# Fruits.append("orange")
+# Fruits.insert(1 , "blueberry")
+# more_fruits = ["Mango" , "Grapes" , "Guavava"]
+# Fruits.extend(more_fruits)
+# print(Fruits)
+
+
+# # Removed
+
+# popped_fruits = Fruits.pop(1)
+# print(Fruits)
+# print(popped_fruits)
+
+
+
+# # tuple
+
+# corrdinates = (10 , 20)
+# x = corrdinates[0]
+# y = corrdinates[1]
+# length = len(corrdinates)
+# # print(x)
+# # print(y)
+# # print(length)
+
+# print("Plus:" , corrdinates[0] + corrdinates[1])
+# print("Sum:" ,corrdinates[0] * corrdinates[1])
+# print("Divide:" ,corrdinates[0] / corrdinates[1])
+# print("Modulas:" ,corrdinates[0] % corrdinates[1])
+# print("Expo:" ,corrdinates[0] ** corrdinates[1])
+# print("Floor:" ,corrdinates[0] // corrdinates[1])
+
+# # Assignment Operators
+# print("LessThan:" ,corrdinates[0] < corrdinates[1])
+# print("GreaterThan:" ,corrdinates[0] > corrdinates[1])
+# print("EqualsTo:" ,corrdinates[0] == corrdinates[1])
+# print("Not EqualsTo:" ,corrdinates[0] != corrdinates[1])
+# print("Lessthan Equalto:" ,corrdinates[0] <= corrdinates[1])
+# print("Greaterthan equalsto:" ,corrdinates[0] >= corrdinates[1])
+
+# x = 9
+# print(x)
+# print(x + 1)
+# print(x)
+
+# ExcellenceAI = 25000
+# Miss = 25000
+# rent = 36000
+# food = 48000
+# Pocket = 20000
+# print(ExcellenceAI + Miss + rent + food + Pocket)
+
+# Cookies = int(input())
+# Friends = int(input())
+
+# l = Cookies // Friends
+# cookies_bache = Cookies % Friends
+# print(l , cookies_bache)
+# # print(cookies_bache)
+
+
+# s1 = "hello"
+# s2 = "world"
+
+# repeated = s1 * 3
+# print(repeated)
+
+
+
+# c = int(input())
+# f = int(input())
+
+# d = c / f
+# r = c % f
+# print(d, r)
+
+
+# c = int(input())
+# f = int(input())
+# print(c//f)
+
+# x = 10
+# x **= 2
+# print(x)
+
+
+# a = 12  # Example value for 'a'
+# b = 7   # Example value for 'b'
+
+# # Bitwise left shift for 'a'
+# print(a<< 1)
+
+# # Bitwise right shift for 'b'
+# print(b>>1
+# )
+
+
+# print of sum n natural numbers
+num = 1
+N = int(input()) #user input
+s = 0 #this will store our final sum
+
+while num <= N:
+    # continiuos progression s being incremet and also n
+    print(s, num)
+    s += num # whatever is current natural number, add to s.
+    num += 1 # Moved to the next natural number
+    #loops end print(s)
+print(s)
+
+
+
+num = 1
+N = int(input()) #user input
+s = 0 #this will store our final sum
+
+while num <= N:
+    s 
